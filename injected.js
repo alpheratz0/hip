6ade11b447a8c1e93c965e2bc792a1a634ce0cb0
alpheratz0 <alpheratz99@protonmail.com>
2022-11-09 22:37:19 -0300

    display more information about the ip address

--- injected.js
+++ injected.js
@@ -21,6 +21,16 @@ const waitForElm = (selector) => {
 	});
 };
 
+const getIPAddressInfo = async (ipAddr) => {
+	const resp = await fetch(`https://ipwho.is/${ipAddr}?fields=country,city,flag.emoji`);
+	const json = await resp.json();
+	return {
+		country: json.country,
+		city: json.city,
+		flag: json.flag.emoji
+	};
+}
+
 const main = () => {
 	const _RTCIceCandidatePrototype = window.RTCIceCandidate.prototype;
 
@@ -29,10 +39,13 @@ const main = () => {
 
 		if (null != ip.match(/(?:(?:[0-9]+)\.){3}[0-9]+/)) {
 			waitForElm('div.chatbox-view > div.log.ps').then((chatbox) => {
-				console.log(`IP: ${ip}`);
-				const p = document.createElement('p');
-				p.innerText = `IP: ${ip}`;
-				chatbox.append(p);
+				getIPAddressInfo(ip).then(info => {
+					const msg = `IP: ${ip}\nCountry: ${info.flag} (${info.country})\nCity: ${info.city}`;
+					const p = document.createElement('p');
+					p.innerText = msg;
+					chatbox.append(p);
+					console.log(msg);
+				});
 			});
 		}
 
