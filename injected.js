ad767c145c05906936b365b9ef4e7978e2121d18
alpheratz0 <alpheratz99@protonmail.com>
2022-11-09 22:17:05 -0300

    display a message in the chat with the ip information

--- injected.js
+++ injected.js
@@ -1,10 +1,45 @@
-const _RTCIceCandidatePrototype = window.RTCIceCandidate.prototype;
+const waitForElm = (selector) => {
+	return new Promise((resolve) => {
+		let queryResult, observer;
 
-window.RTCIceCandidate = function(candidateInfo) {
-	const ip = candidateInfo.candidate.split(' ')[4];
-	if (null != ip.match(/(?:(?:[0-9]+)\.){3}[0-9]+/))
-		console.log(ip);
-	return new _RTCIceCandidatePrototype.constructor(candidateInfo);
-}
+		queryResult = document.querySelector(selector);
 
-window.RTCIceCandidate.prototype = _RTCIceCandidatePrototype;
+		if (queryResult) {
+			return resolve(queryResult);
+		}
+
+		observer = new MutationObserver((mutations) => {
+			queryResult = document.querySelector(selector);
+
+			if (queryResult) {
+				resolve(queryResult);
+				observer.disconnect();
+			}
+		});
+
+		observer.observe(document.body, { childList: true, subtree: true });
+	});
+};
+
+const main = () => {
+	const _RTCIceCandidatePrototype = window.RTCIceCandidate.prototype;
+
+	window.RTCIceCandidate = function (candidateInfo) {
+		const ip = candidateInfo.candidate.split(' ')[4];
+
+		if (null != ip.match(/(?:(?:[0-9]+)\.){3}[0-9]+/)) {
+			waitForElm('div.chatbox-view > div.log.ps').then((chatbox) => {
+				console.log(`IP: ${ip}`);
+				const p = document.createElement('p');
+				p.innerText = `IP: ${ip}`;
+				chatbox.append(p);
+			});
+		}
+
+		return new _RTCIceCandidatePrototype.constructor(candidateInfo);
+	};
+
+	window.RTCIceCandidate.prototype = _RTCIceCandidatePrototype;
+};
+
+main();
