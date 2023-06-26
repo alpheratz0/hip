d71f5a04731bb41ec8f60563edd3f9bb0ab06e87
alpheratz0 <alpheratz99@protonmail.com>
2023-06-26 20:32:14 -0300

    fix ip grabber

--- injected.js
+++ injected.js
@@ -1,77 +1,39 @@
 /*
-	Copyright (C) 2022 <alpheratz99@protonmail.com>
+	Copyright (C) 2022-2023 <alpheratz99@protonmail.com>
 
-	This program is free software; you can redistribute it and/or modify it under
-	the terms of the GNU General Public License version 2 as published by the
-	Free Software Foundation.
+	This program is free software; you can redistribute it and/or modify it
+	under the terms of the GNU General Public License version 2 as published by
+	the Free Software Foundation.
 
-	This program is distributed in the hope that it will be useful, but WITHOUT ANY
-	WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-	FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+	This program is distributed in the hope that it will be useful, but WITHOUT
+	ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+	FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+	more details.
 
-	You should have received a copy of the GNU General Public License along with
-	this program; if not, write to the Free Software Foundation, Inc., 59 Temple
-	Place, Suite 330, Boston, MA 02111-1307 USA
+	You should have received a copy of the GNU General Public License along
+	with this program; if not, write to the Free Software Foundation, Inc., 59
+	Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 */
 
-const waitForElm = (selector) => {
-	return new Promise((resolve) => {
-		let queryResult, observer;
+console.clear();
 
-		queryResult = document.querySelector(selector);
+window.originalRTCPeerConnection = window.RTCPeerConnection;
 
-		if (queryResult) {
-			return resolve(queryResult);
+window.RTCPeerConnection = function (...args) {
+	const conn = new window.originalRTCPeerConnection(...args);
+	conn.originalAddIceCandidate = conn.addIceCandidate;
+	conn.addIceCandidate = function (iceCandidate, ...args) {
+		const candidateInfo = iceCandidate.candidate.split(" ");
+		const candidateAddress = candidateInfo[4];
+		const candidateType = candidateInfo[7];
+
+		if (candidateType == "srflx") {
+			console.log("%c%s", "color:blue;", candidateAddress);
 		}
 
-		observer = new MutationObserver((mutations) => {
-			queryResult = document.querySelector(selector);
+		return conn.originalAddIceCandidate(iceCandidate, ...args);
+	}
 
-			if (queryResult) {
-				resolve(queryResult);
-				observer.disconnect();
-			}
-		});
-
-		observer.observe(document.body, { childList: true, subtree: true });
-	});
-};
-
-const getIPAddressInfo = async (ipAddr) => {
-	const resp = await fetch(`https://ipwho.is/${ipAddr}?fields=country,city,flag.emoji`);
-	const json = await resp.json();
-	return {
-		country: json.country || 'Unknown',
-		city: json.city || 'Unknown',
-		flag: json.flag?.emoji || '??'
-	};
+	return conn;
 }
-
-const main = () => {
-	const _RTCIceCandidatePrototype = window.RTCIceCandidate.prototype;
-
-	window.RTCIceCandidate = function (candidateInfo) {
-		const instance = new _RTCIceCandidatePrototype.constructor(candidateInfo);
-		const ip = instance.address;
-		const port = instance.port;
-
-		if (null != ip.match(/(?:(?:[0-9]+)\.){3}[0-9]+/)) {
-			waitForElm('div.chatbox-view > div.log.ps').then((chatbox) => {
-				getIPAddressInfo(ip).then(info => {
-					const msg = `IP: ${ip} Port: ${port} From: ${info.city}, ${info.flag} ${info.country}`;
-					const p = document.createElement('p');
-					p.innerText = msg;
-					chatbox.append(p);
-					console.log(msg);
-				});
-			});
-		}
-
-		return instance;
-	};
-
-	window.RTCIceCandidate.prototype = _RTCIceCandidatePrototype;
-};
-
-main();
